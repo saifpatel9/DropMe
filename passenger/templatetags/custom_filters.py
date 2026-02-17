057100caffc9d42a70c60b82ba5d6d983908efda
Saif Patel <saifpatel9@gmail.com>
2026-02-17 14:17:28 +0530

    `Updated passenger templates and custom filters`

--- passenger/templatetags/custom_filters.py
+++ passenger/templatetags/custom_filters.py
@@ -5,3 +5,11 @@ register = template.Library()
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+
+@register.filter
+def before_comma(value):
+    if value is None:
+        return ""
+    text = str(value)
+    return text.split(",", 1)[0].strip()
